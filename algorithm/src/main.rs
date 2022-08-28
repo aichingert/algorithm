81b7b4b58d41746a4f1a23ce75a943993449e5c6
aichingert <aichinger.tobias13@gmail.com>
2022-08-28 16:51:02 +0200

    Implemented Node

--- algorithm/src/main.rs
+++ algorithm/src/main.rs
@@ -1,3 +1,7 @@
+
+
+mod node;
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ algorithm/src/node.rs
@@ -0,0 +1,14 @@
+
+pub struct Node {
+    value: i32,
+    connections: Vec<Option<Box<Node>>>
+}
+
+impl Node {
+    fn new(value: i32, connections: Vec<Option<Box<Node>>>) -> Self {
+        Self {
+            value,
+            connections
+        }
+    }
+}
