4ace3b37e4f0491b5e664131433cd5fd15f0dcbd
aichingert <aichinger.tobias13@gmail.com>
2022-08-29 18:00:12 +0200

    Fighting the input

--- dijkstra/src/dijkstra.rs
+++ dijkstra/src/dijkstra.rs
@@ -1,7 +1,9 @@
 use crate::node::Node;
 
 pub struct Dijkstra {
+    pub current_node_id: String,
     pub current_node_value: String,
+    pub current_node_connections: String,
     pub nodes: Vec<Node>
 }
 
@@ -13,7 +15,9 @@ impl Dijkstra {
 
     pub fn default() -> Self {
         Self {
+            current_node_id: "".to_string(),
             current_node_value: "".to_string(),
+            current_node_connections: "".to_string(),
             nodes: vec![]
         }
     }
@@ -24,7 +28,7 @@ impl std::fmt::Debug for Dijkstra {
         let mut output: String = String::new();
 
         for i in 0..self.nodes.len() {
-            output.push_str(&self.nodes[i].to_string());
+            output.push_str(&format!("{:?} \n", self.nodes[i].to_string()));
         }
         write!(f, "{output}")
     }
--- dijkstra/src/node.rs
+++ dijkstra/src/node.rs
@@ -1,25 +1,35 @@
 pub struct Node {
-    value: i32,
-    connections: Vec<Option<Box<Node>>>
+    pub id: u16,
+    pub value: i32,
+    pub connections: Vec<Option<Box<Node>>>
 }
 
 impl Node {
-    pub fn new(value: i32, connections: Vec<Option<Box<Node>>>) -> Self {
+    pub fn new(id: u16, value: i32, connections: Vec<Option<Box<Node>>>) -> Self {
         Self {
+            id,
             value,
             connections
+        }
+    }
+
+    pub fn default() -> Self {
+        Self {
+            id: 0,
+            value: 0,
+            connections: vec![]
         }
     }
 }
 
 impl std::fmt::Debug for Node {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Node: {} - {:?} \n", self.value, self.connections)
+        write!(f, "Node {{ id: {} - value: {} - connections: {:?} }}", self.id, self.value, self.connections)
     }
 }
 
 impl std::fmt::Display for Node {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Node: {} - {:?} \n", self.value, self.connections)
+        write!(f, "Node {{ id: {} - value: {} - connections: {:?} }}", self.id, self.value, self.connections)
     }
 }
