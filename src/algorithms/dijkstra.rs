ac11268572d4394863bfc54b7d8d9718e0ad5049
aichingert <aichinger.tobias13@gmail.com>
2023-09-07 18:22:51 +0200

    feat: able to draw some nice circles

--- src/algorithms/dijkstra.rs
+++ src/algorithms/dijkstra.rs
@@ -1,23 +1,37 @@
+use std::f64::consts::PI;
+
 use leptos::*;
 use wasm_bindgen::prelude::*;
 
 use crate::components::Circle;
+
+const R: f64 = 32.0;
 
 #[component]
 pub fn Dijkstra(cx: Scope) -> impl IntoView {
     let canvas_ref = create_node_ref::<html::Canvas>(cx);
 
     canvas_ref.on_load(cx, move |canvas: HtmlElement<html::Canvas>| {
-        let js_value = canvas.get_context("2d");
+        let canvas_cln: HtmlElement<html::Canvas>= canvas.clone();
 
-        if let Ok(object) = js_value {
-            if let Some(obj) = object {
-                if let Ok(ctx) = obj.dyn_into::<web_sys::CanvasRenderingContext2d>() {
-                    ctx.fill_rect(100.0, 100.0, 200.0, 200.0);
+        canvas.set_height(800);
+        canvas.set_width(800);
+
+        canvas.on(ev::click, move |e| {
+            let js_value = canvas_cln.get_context("2d");
+            let x: f64 = e.x().into();
+            let y: f64 = e.y().into();
+
+            if let Ok(object) = js_value {
+                if let Some(obj) = object {
+                    if let Ok(ctx) = obj.dyn_into::<web_sys::CanvasRenderingContext2d>() {
+                        ctx.begin_path();
+                        _ = ctx.arc(x - 10.0, y - R, R, 0.0, 2. * PI);
+                        ctx.stroke();
+                    }
                 }
-
             }
-        }
+        });
     });
 
     view! { cx,
